0953546fc75f6b8294b51ff79b662a6506e5e835
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2021-12-04 10:56:19 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,11 +8,11 @@ addLayer("pa", {
     }},
     color: "#A600FF",
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "partial points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -22,7 +22,7 @@ addLayer("pa", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Reset for partial points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
 	upgrades: {
