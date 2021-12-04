f7423350e85703f16440a429635bdd646ca7c29b
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2021-12-04 10:51:15 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+	upgrades: {
+		11: {
+			title: "Partial Production",
+    		description: "Start gaining points.",
+    		cost: new Decimal(1),
+        },
+    },
 })
