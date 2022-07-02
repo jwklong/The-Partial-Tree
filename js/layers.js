16981849115dddaa263a89ec15ada8dcffda171e
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-07-02 10:43:10 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,12 @@
 addLayer("ma", {
-    name: "Mass", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-	points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){console.log("nothing")}},
-    ],
-    layerShown(){return true}
+	name: "Mass", // This is optional, only used in a few places, If absent it just uses the layer id.
+	symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
+	position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+	startData() { return {
+		unlocked: true,
+		points: new Decimal(0),
+	}},
+	color: "#4BDC13",
+	row: 0, // Row the layer is in on the tree (0 is the first row)
+	layerShown(){return true}
 })
