b060c05fe047a6b8f264c5745035015b1a8fd76f
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-07-05 20:14:51 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -26,7 +26,7 @@ addLayer("ma", {
 				var x = new Decimal(10).mul(new Decimal(1.5).pow(getBuyableAmount(this.layer, this.id)))
 				return x
 			},
-			display() { return `Muscler [${!getBuyableAmount(this.layer, this.id).eq(0) ? getBuyableAmount(this.layer, this.id).toStringWithDecimalPlaces(3) : "0"}]\n\nCost: ${this.cost().toStringWithDecimalPlaces(3)}\nEffect: +${this.cost().toStringWithDecimalPlaces(3)}` },
+			display() { return `Muscler [${!getBuyableAmount(this.layer, this.id).eq(0) ? getBuyableAmount(this.layer, this.id).toStringWithDecimalPlaces(3) : "0"}]\n\nCost: ${this.cost().toStringWithDecimalPlaces(3)}\nEffect: +${this.effect().toStringWithDecimalPlaces(3)}` },
 			canAfford() { return player.points.gte(this.cost()) },
 			buy() {
 				player.points = player.points.sub(this.cost())
